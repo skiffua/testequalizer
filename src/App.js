0025e2019aaa098faae80c3db49cd134eaf1d9ff
Oleh <oleh_mushora@ukr.net>
2019-06-12 21:58:26 +0300

    final first stage

--- src/App.js
+++ src/App.js
@@ -22,19 +22,20 @@ class App extends React.Component {
   
     var numPoints = analyser.frequencyBinCount-80;
     var heightArray = new Uint8Array(numPoints);
-  
+        
     function render() {
       analyser.getByteFrequencyData(heightArray);
       var width = ctx.canvas.width;
       var height = ctx.canvas.height;
       var countcolumns=Math.floor(ctx.canvas.width/52);
-      var columnwidth=Math.floor(7/8*ctx.canvas.width/52)
+      var columnwidth=Math.floor(5/6*ctx.canvas.width/52);
+      
       ctx.clearRect(0, 0, width, height);
       for (var x =0; x < width; x += countcolumns) {
          var ndx = x * numPoints / width | 0;
          var vol = heightArray[ndx];
          var y = vol * height / 512;            
-         roundedRect(ctx, x, height/2, columnwidth, y, 3)     
+         roundedRect(ctx, x, height/2, columnwidth, y, 2)          
       }    
       requestAnimationFrame(render);
     }
@@ -51,12 +52,13 @@ class App extends React.Component {
       ctx.arcTo(x + width, y-height, x+width-radius, y - height, radius);
       ctx.lineTo(x +radius, y-height);
       ctx.arcTo(x, y - height, x, y - height+radius, radius);
+      // ctx.stroke();
       ctx.fillStyle =flagColorColumn ? '#1ecea8' : '#93969f';
       flagColorColumn =!flagColorColumn;
       ctx.fill();      
-    }  
-  
-     // Make a audio node
+    } 
+    
+        // Make a audio node
     var audio = new Audio();
     audio.loop = true;
     audio.autoplay = true;
