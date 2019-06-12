8d30bf71be6592b81889c7ada535669824a73640
Oleh <oleh_mushora@ukr.net>
2019-06-12 09:50:06 +0300

    work withuot bug

--- src/App.js
+++ src/App.js
@@ -7,8 +7,12 @@ import sound from './jack.mp3'
 
 
 class App extends React.Component {
+  
+  state={widthCanvas: 400}
+  
+ 
   componentDidMount(){
-    
+   
     function start(){
     var context = new (window.AudioContext || window.webkitAudioContext)();
     var analyser = context.createAnalyser();
@@ -16,27 +20,21 @@ class App extends React.Component {
     var ctx = document.querySelector("canvas").getContext("2d");
     var flagColorColumn=true;
   
-    var numPoints = analyser.frequencyBinCount;
+    var numPoints = analyser.frequencyBinCount-80;
     var heightArray = new Uint8Array(numPoints);
   
     function render() {
       analyser.getByteFrequencyData(heightArray);
       var width = ctx.canvas.width;
       var height = ctx.canvas.height;
+      var countcolumns=Math.floor(ctx.canvas.width/52);
+      var columnwidth=Math.floor(7/8*ctx.canvas.width/52)
       ctx.clearRect(0, 0, width, height);
-      for (var x =0; x < width; x += 10) {
+      for (var x =0; x < width; x += countcolumns) {
          var ndx = x * numPoints / width | 0;
          var vol = heightArray[ndx];
-         var y = vol * height / 255;    
-        //  ctx.fillStyle = flagColorColumn ? '#1ecea8' : '#93969f';
-        //  flagColorColumn =!flagColorColumn;
-          
-        //  roundedRect(ctx, x, height/2, 10, y, 3)
-         roundedRect(ctx, x, height/2, 10, y, 3)
-
-        //  ctx.fillRect(x, height/2, 10, y);
-        //  ctx.fillRect(x, height/2, 10, -y);
-      
+         var y = vol * height / 512;            
+         roundedRect(ctx, x, height/2, columnwidth, y, 3)     
       }    
       requestAnimationFrame(render);
     }
@@ -44,9 +42,6 @@ class App extends React.Component {
     
     function roundedRect(ctx, x, y, width, height, radius) {
       ctx.beginPath();
-      ctx.strokeStyle = 'orange';
-     
-      context.lineWidth = 1;     
       ctx.moveTo(x, y-height);
       ctx.lineTo(x, y +height - radius);
       ctx.arcTo(x, y + height, x + radius, y + height, radius);
@@ -56,13 +51,9 @@ class App extends React.Component {
       ctx.arcTo(x + width, y-height, x+width-radius, y - height, radius);
       ctx.lineTo(x +radius, y-height);
       ctx.arcTo(x, y - height, x, y - height+radius, radius);
-      // ctx.lineTo(x + radius, y);
-      // /actx.closePath();
       ctx.fillStyle =flagColorColumn ? '#1ecea8' : '#93969f';
       flagColorColumn =!flagColorColumn;
-      ctx.fill();
-      ctx.strokeStyle = 'orange';
-      ctx.stroke();
+      ctx.fill();      
     }  
   
      // Make a audio node
@@ -87,22 +78,30 @@ class App extends React.Component {
     audio.play();    
   }
   var clickElem = document.querySelector("button");
-    // console.log(clickElem)
-    clickElem.addEventListener('click', function() {
+      clickElem.addEventListener('click', function() {
       clickElem.style.display = 'none';
       start();
     });
-  }
-
+  // var rangeElement=document.getElementById('range');
+  //     rangeElement.addEventListener('change', function(e){
+  //     console.log(widthMerge.call(App))
+  //     // console.log(this.state)
+  //     // this.setState={widthCanvas:100}
+  //     }
+  //     )
+    
+ 
+  };
+  widthMerge=(e)=>{
+    console.log(this.state) 
+    this.setState({widthCanvas: e.target.value})
+  }     
+  
   render(){
   return (
     <div className="App">
-      <figure  >
-        <figcaption>Listen to the Calvin Harris:</figcaption>
-        <audio id="rootAudio" controls src={sound}>                
-        </audio>
-      </figure>
-      <Equaliser width="600" height="200" src={sound}/>
+      <Equaliser width={this.state.widthCanvas} height="200" src={sound} onchange={this.widthMerge}/>
+      
     </div>
   );
   }
--- src/equalizer.js
+++ src/equalizer.js
@@ -11,6 +11,9 @@ function equlizer(props){
             <canvas id="graphicequalizer" width={props.width} height={props.height} src={props.src}
                     style={{borderColor: "green", borderWidth: "2px", borderStyle: "solid"}}
             > </canvas> 
+            <br />
+            <input type="range" min="100" max="1000" step="10" 
+                    defaultValue="400" name="rangecanvas" id="range" onChange={props.onchange} />
         </div>    
     )
 }
